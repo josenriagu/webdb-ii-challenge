ffe639728675e442a4737955f736ed7139797545
Josemaria <josemarianriagu25@gmail.com>
2019-07-16 20:07:38 +0800

    create and run cars table migration

---
+++ data/migrations/20190716193014_createCarsTable.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex) {
+   return knex.schema.createTable('cars', table => {
+      table.increments();
+      table.string('VIN', 128).notNullable();
+      table.string('make', 128).notNullable();
+      table.string('model', 128).notNullable();
+      table.string('mileage', 128).notNullable();
+      table.string('transmission', 128);
+      table.string('status', 128);
+      table.timestamps();
+  })
+};
+
+// down function provides the reverse of the up function
+exports.down = function(knex) {
+   return knex.schema.dropTableIfExists('cars');
+};
